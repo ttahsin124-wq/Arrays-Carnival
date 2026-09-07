e72e6e8b76a8fed67e03e12aaff2ac3f850ed98c
Anisa Tahsin Nisa <ttahsin124@gmail.com>
2026-09-07 20:00:36 +0600

    Time: 0 ms (100.00%) | Memory: 20.8 MB (24.15%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -4,7 +4,7 @@ public:
     int result=0;
     for(int num:nums)
     {
-        result^=num;
+        result^=num;// when pair needs to cancel and one value needs to survive XOR is the solution because it makes all the pair disappear
     }
     return result;
     }
