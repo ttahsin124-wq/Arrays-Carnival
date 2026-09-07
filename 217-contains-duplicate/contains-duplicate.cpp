290df3fb48735ab29f0a004f31534569187081de
Anisa Tahsin Nisa <ttahsin124@gmail.com>
2026-09-07 19:10:41 +0600

    Time: 152 ms (5.34%) | Memory: 113.7 MB (8.52%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      set<int>s;
+      for(int num:nums)
+      {
+        if(s.count(num))
+        {
+            return true;
+        }
+        s.insert(num);
+      }  
+      return false;
+    }
+};
